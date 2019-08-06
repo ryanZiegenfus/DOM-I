ac17028e985a42f9b25caac20866b8e3eb302df8
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-08-05 23:58:10 -0400

    completed mvp goals for DOM-I

--- js/index.js
+++ js/index.js
@@ -39,4 +39,66 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navButtons = document.querySelectorAll("a");
+  navButtons.forEach((currentValue, index) => currentValue.textContent = siteContent.nav[`nav-item-${index + 1}`]);
+
+
+
+let DOMbr = siteContent.cta.h1.split(" ").map(element => {
+  let spanElement = document.createElement('span');
+  spanElement.textContent = element;
+  return spanElement;
+})
+
+let introH1 = document.querySelector(".cta-text h1");
+
+console.log(DOMbr)
+DOMbr.forEach((element) => introH1.append(element));
+
+document.querySelectorAll('span').forEach(element => element.style.display = 'block');
+
+let ctaImg = document.querySelector("#cta-img");
+ctaImg.src = siteContent.cta["img-src"];
+
+let ctaButton = document.querySelector(".cta-text button");
+ctaButton.textContent = siteContent.cta.button;
+
+let mainArray = ['features', 'about', 'services', 'product', 'vision'];
+
+let mainH4 = document.querySelectorAll('.text-content h4');
+mainH4.forEach((currentValue, index) => currentValue.textContent = siteContent["main-content"][`${mainArray[index]}-h4`]);
+
+let mainp = document.querySelectorAll('.text-content p');
+mainp.forEach((currentValue, index) => currentValue.textContent = siteContent["main-content"][`${mainArray[index]}-content`]);
+
+let midImg = document.querySelector('#middle-img');
+midImg.src = siteContent["main-content"]["middle-img-src"];
+
+let contactH4 = document.querySelector('.contact h4');
+contactH4.textContent = siteContent.contact["contact-h4"];
+
+let contactArray = ['address', 'phone', 'email'];
+
+let contactp = document.querySelectorAll('.contact p');
+contactp.forEach((currentValue, index) => currentValue.textContent = siteContent.contact[`${contactArray[index]}`]);
+contactp[0].style.width = '20%';
+
+let footerp = document.querySelector('footer p');
+footerp.textContent = siteContent.footer.copyright;
+
+navButtons.forEach((element) => element.style.color = "green");
+
+let firsta = document.createElement('a');
+firsta.textContent = 'Home';
+firsta.style.color = 'green';
+
+let lasta = document.createElement('a');
+lasta.textContent = 'Subscribe';
+lasta.style.color = 'green';
+
+let navBar = document.querySelector('nav');
+navBar.prepend(firsta);
+navBar.appendChild(lasta);
+
